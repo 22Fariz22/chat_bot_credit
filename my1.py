75a2412ba09fc153c3361985bd4d104fb3822c79
Fariz Rustamov <49908901+22Fariz22@users.noreply.github.com>
2020-10-27 18:43:42 +0300

    Update my1.py

--- my1.py
+++ my1.py
@@ -1,22 +1,39 @@
 # -*- coding: utf-8 -*-
 import telebot
-
+# 1391358816:AAFCQLtieoSlxYmdNdOFZ4eIhEGWKoju3T4
 import telebot
+from json_write_function import write_json
 
-bot = telebot.TeleBot('вставить свой токен')
+
+bot = telebot.TeleBot('тут токен, спросите у меня')
 sum_credit = 0
 
-phone_num = {'89167345200':'Уважаемая Екатерина Юрьевна!', '89288167447': 'Уважаемый Денис Олегович!','89262682738': 'Уважаемый Фариз Фазилевич!'}
+phone_num = {тут список номеров типа '89161234567':'Иван Петров',}
+name = ''
 client_number = ''
+credit = ''
+user_id = ''
+person = {
+    'name': name,
+    'phone_number': str(client_number),
+    'credit': str(credit),
+    'user_id': user_id
+    }
+
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "введите команду /credit")
-
+num = ''
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
+    global num
     if message.text == '/credit':
         bot.send_message(message.from_user.id, "Здравствуйте! Пожалуйста введите свой телефонный номер в любом формате")
-        bot.register_next_step_handler(message, reg_name)
+        num = bot.register_next_step_handler(message, reg_name)
+
+        # попробуем вывести и сохранить куда нибудь тел номер
+
 flag = False
 def reg_name(message):
     global client_number
@@ -27,6 +44,9 @@ def reg_name(message):
         if k.replace("-", "").replace(" ", "")[-10:] == client_number:
             print(bot.send_message(message.from_user.id, f"{v},введите сумму не превышающую 100.000 рублей"))
             flag = True
+            person['name'] = v
+            person['phone_number'] = k
+
     if flag == False:
         print(bot.send_message(message.from_user.id,"Извините, ваш номер не найден. Вам надо пройти регистрацию"))
 
@@ -42,7 +62,10 @@ def reg_sum(message):
     else:
         bot.send_message(message.from_user.id, "Ваш запрос отправлен в банк,"
                                               " ожидайте ответа.Спасибо.")
+        person['credit'] = str(message.text)
+
+        write_json(person)
+
 
 bot.polling()
-
 
