c7668e2cbfe6d5de656e4b029d8fefec04e233d4
Vladislav Osadchy <vladislav.osadchy@gmail.com>
2020-10-28 11:32:52 +0300

    Добавил функцию переноса словарей на базу vd_db в коллекцию vd_telegram_bot на Mongo

--- json_write_function.py
+++ json_write_function.py
@@ -4,22 +4,16 @@ import os
 
 def write_json(person_dict):
     try:
-        json.load(open('person.json'))
+        with open('persons.json') as f:
+            data = json.load(f)
     except:
         data = []
 
     data.append(person_dict)
 
-    with open('persons.json', 'a', encoding='utf-8') as file:
+    with open('persons.json', 'w', encoding='utf-8') as file:
         json.dump(data, file, indent=2, ensure_ascii=False)
-        file.write(os.linesep)
 
-    # with open('persons.json', encoding='utf-8') as file:
-    #     json.dump(data, file, indent=2, ensure_ascii=False)
+    
 
-    # with open('persons.json', 'a') as file:
-    #     json.dump(data, file, indent=2, ensure_ascii=False)
-    # with open('person_dict.json', 'a', encoding='utf-8') as file:
-    #     json.dump(person_dict, file, ensure_ascii=False)
-    data = []
 
